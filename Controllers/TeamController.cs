0e9e9c9018aee191ddc0ac58acc9994f5384042a
AdamASchulte <adamaschulte@yahoo.com>
2023-11-07 22:30:23 -0600

    Initializing project and testing out spectre

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,39 @@
+﻿namespace FootballTeams.Controllers;
+
+public class TeamController
+{
+    public static void AddTeam()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void AddLocation()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void AddPlayers()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void AddPlayerStats()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void AddStadium()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void AddStaff()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void AddTeamStats()
+    {
+        throw new NotImplementedException();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FootballTeams.Controllers;
+using Spectre.Console;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var appIsRunning = true;
+
+        while (appIsRunning)
+        {
+            var queryOption = AnsiConsole.Prompt(
+            new SelectionPrompt<OuterMenuOptions>()
+            .Title("What Team would you like to look at?")
+            .AddChoices(
+                OuterMenuOptions.Teams,
+                OuterMenuOptions.Players,
+                OuterMenuOptions.Staff,
+                OuterMenuOptions.Location,
+                OuterMenuOptions.Stadium,
+                OuterMenuOptions.TeamStats,
+                OuterMenuOptions.PlayerStats));
+
+            switch (queryOption)
+            {
+                case OuterMenuOptions.Teams:
+                    //Really we will once again prompt the user to see what they want to do with the team
+                    TeamController.AddTeam();
+                    break;
+                case OuterMenuOptions.Players:
+                    TeamController.AddPlayers();
+                    break;
+                case OuterMenuOptions.Staff:
+                    TeamController.AddStaff();
+                    break;
+                case OuterMenuOptions.Location:
+                    TeamController.AddLocation();
+                    break;
+                case OuterMenuOptions.Stadium:
+                    TeamController.AddStadium();
+                    break;
+                case OuterMenuOptions.TeamStats:
+                    TeamController.AddTeamStats();
+                    break;
+                case OuterMenuOptions.PlayerStats:
+                    TeamController.AddPlayerStats();
+                    break;
+            }
+        }
+    }
+
+    enum OuterMenuOptions
+    {
+        Teams,
+        Players,
+        Staff,
+        Location,
+        Stadium,
+        TeamStats,
+        PlayerStats
+    }
+
+}
