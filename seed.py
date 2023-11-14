7178c6ec9ab2f14f4ffa98c480381d88369b9dd6
SealyBoi <aiden.mathews2002@gmail.com>
2023-11-13 20:39:18 -0600

    Cleaning Python

--- seed.py
+++ seed.py
@@ -5,25 +5,20 @@ print("Connecting to db...")
 con = sqlite3.connect("./FootballTeams.db")
 print("Connected!")
 cur = con.cursor()
-print("Selecting names...")
-table_list = [a for a in cur.execute("SELECT name FROM sqlite_master WHERE type = 'table'")]
-print(table_list)
 
-guid = str(uuid.uuid4())
-city = "Baton Rouge"
-state = "Louisiana"
-
-guid2 = str(uuid.uuid4())
-city2 = "Branson"
-state2 = "Missouri"
+###Inserting into db###
+#guid = str(uuid.uuid4())
+#city = "Baton Rouge"
+#state = "Louisiana"
 
 #cur.execute("""
 #    INSERT INTO Locations VALUES
 #            (?, ?, ?)
-#""", (guid2, city2, state2))
+#""", (guid, city, state))
 
 #con.commit()
 
+###Querying db###
 res = [a for a in cur.execute("SELECT * FROM Locations")]
 print(res)
 
